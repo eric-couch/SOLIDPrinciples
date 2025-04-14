002eaf4805b993d8914b7d46a55ba670ff9baa78
Eric Couch <ericcouch@yahoo.com>
2025-04-14 11:03:10 -0500

    added program.cs notes for teaching

--- SOLIDPrinciples/Program.cs
+++ SOLIDPrinciples/Program.cs
@@ -1,5 +1,38 @@
-﻿using SOLIDPrinciples.D.GoodExmaple;
+﻿//using SOLIDPrinciples.L.BadExample;
+
+//// replace with Square to show that it doesn't work (bad example)
+//Rectangle rect = new Rectangle();
+
+//rect.Width = 10;
+//rect.Height = 5;
+//Console.WriteLine("Expected area = 10 * 5 = 50");
+//Console.WriteLine($"Calculated area = {rect.Area}");
 
 
-Car car = new Car(new ElectricMotor());
+//using SOLIDPrinciples.L.GoodExample;
+
+//Shape rectangle = new Rectangle { Width = 5, Height = 4 };
+//Console.WriteLine($"Area of rectangle: {rectangle.Area}");
+
+//Shape square = new Square { SideLength = 5 };
+//Console.WriteLine($"Area of square: {square.Area}");
+
+
+//using SOLIDPrinciples.I.BadExample;
+
+//var circle = new circle();
+//circle.Radius = 10;
+//Console.WriteLine(circle.Area());
+//Console.WriteLine(circle.Volume());
+
+
+
+//using SOLIDPrinciples.D.GoodExmaple;
+
+
+//Car car = new Car(new ElectricMotor());
+
+
+
+
 
